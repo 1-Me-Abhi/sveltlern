ae5b975dd0d55511974909ae9ead41fe1c203294
Abhi <sudo.apt.abhi@gmail.com>
2026-01-11 21:39:08 +0530

    first

---
+++ src/App.svelte
@@ -0,0 +1,36 @@
+<!-- <script>
+  import Header from "./component/header.svelte";
+
+  let name = "Abhi";
+</script>
+
+<Header  age={30} location="New York" />
+<div>
+  <label for="name">Name</label>
+  <input type="text" bind:value={name} id="name" />
+  <p>hello {name}</p>
+</div> -->
+
+<script>
+  let count=$state(0);
+  let increment = () => {
+    count = count + 1;
+  };
+  let decrement = () => {
+    count = count - 1;
+  };
+</script>
+<h1>Count :{count}</h1>
+<button onclick={increment}>Increment</button>
+<button onclick={decrement}>Decrement</button><script>
+  import Header from "./component/header.svelte";
+
+  let name = "Abhi";
+</script>
+
+<Header  age={30} location="New York" />
+<div>
+  <label for="name">Name</label>
+  <input type="text" bind:value={name} id="name" />
+  <p>hello {name}</p>
+</div>
---
+++ src/component/header.svelte
@@ -0,0 +1,7 @@
+<script>
+    const allProps = $props();
+
+</script>
+<h1>This will display a header</h1>
+<div>Age: {allProps.age}</div>
+<h1>Location : {allProps.location}</h1>
